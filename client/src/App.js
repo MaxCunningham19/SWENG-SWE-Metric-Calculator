2d263afb0874a76f97c07ab58ce089d7e3779a0c
aislinggallagher <gallagheraisling01@gmail.com>
2022-11-27 04:28:12 +0000

    dark mode and attempt at pos and neg chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,84 @@
 import React, {useEffect, useState} from 'react'
+import './components/darkMode.css';
 import Graph from './components/totalCommitsGraph'
 import LinesGraph from './components/linesAddedGraph'
 import DeletedGraph from './components/linesDeletedGraph'
 import MergesGraph from './components/mergesGraph'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import Navbar from './components/Navbar';
-
+const data = [
+  {
+      "username": "declanquinn00",
+      "commits": 8,
+      "merges": 0
+  },
+  {
+      "username": "Max Cunningham",
+      "commits": 15,
+      "lines added": 187488,
+      "lines deleted": 0,
+      "merges": 0
+  },
+  {
+      "username": "francsir",
+      "lines added": 58222,
+      "lines deleted": -43000,
+      "commits": 14,
+      "merges": 0
+  },
+  {
+      "username": "bairdr",
+      "lines added": 164,
+      "lines deleted": -38,
+      "commits": 4,
+      "merges": 0
+  },
+  {
+      "username": "Wiktoria Fabijaniak",
+      "lines added": 29382,
+      "lines deleted": -65,
+      "commits": 2,
+      "merges": 0
+  },
+  {
+      "username": "AAjayiB",
+      "lines added": 0,
+      "lines deleted": 0,
+      "commits": 0,
+      "merges": 0
+  },
+  {
+      "username": "aislinggallagher",
+      "lines added": 0,
+      "lines deleted": 0,
+      "commits": 0,
+      "merges": 0
+  },
+  {
+      "username": "okaforc",
+      "lines added": 0,
+      "lines deleted": 0,
+      "commits": 0,
+      "merges": 0
+  },
+  {
+      "stars": 2,
+      "total_commits": 45,
+      "commits_to_main": 9
+  },
+]; 
 function App() {
 
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }
   {/*const [backendData, setBackendData] = useState([{}])
 
   useEffect(() => {
@@ -19,6 +90,9 @@ function App() {
       }
     )
   }, []) */}
+  useEffect(() => {
+    document.body.className = theme;
+    }, [theme]);
   return (
     <Box>
       <Navbar/>
@@ -32,10 +106,20 @@ function App() {
         ))
       )}
         */}
-<Graph /> 
-<LinesGraph />
-<DeletedGraph />
-<MergesGraph />
+    <Box mt={10}>
+    <Button variant="contained" onClick={toggleTheme}>Toggle Theme</Button>
+    </Box>
+    <Grid
+  container
+  direction="row"
+  justifyContent="center"
+  alignItems="center"
+>
+<Graph data={data}/> 
+<LinesGraph data={data}/>
+{ /* <DeletedGraph data={data}/> */}
+<MergesGraph data={data}/>
+</Grid>
 </Box>
   )
 }
--- client/src/components/mergesGraph.js
+++ client/src/components/mergesGraph.js
@@ -3,68 +3,8 @@ import {useEffect, useState} from 'react'
 import React, { PureComponent } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-    {
-        "username": "declanquinn00",
-        "commits": 8,
-        "merges": 0
-    },
-    {
-        "username": "Max Cunningham",
-        "commits": 15,
-        "lines added": 187488,
-        "lines deleted": 0,
-        "merges": 0
-    },
-    {
-        "username": "francsir",
-        "lines added": 58222,
-        "lines deleted": 43,
-        "commits": 14,
-        "merges": 0
-    },
-    {
-        "username": "bairdr",
-        "lines added": 164,
-        "lines deleted": 38,
-        "commits": 4,
-        "merges": 0
-    },
-    {
-        "username": "Wiktoria Fabijaniak",
-        "lines added": 29382,
-        "lines deleted": 65,
-        "commits": 2,
-        "merges": 0
-    },
-    {
-        "username": "AAjayiB",
-        "lines added": 0,
-        "lines deleted": 0,
-        "commits": 0,
-        "merges": 0
-    },
-    {
-        "username": "aislinggallagher",
-        "lines added": 0,
-        "lines deleted": 0,
-        "commits": 0,
-        "merges": 0
-    },
-    {
-        "username": "okaforc",
-        "lines added": 0,
-        "lines deleted": 0,
-        "commits": 0,
-        "merges": 0
-    },
-    {
-        "stars": 2,
-        "total_commits": 45,
-        "commits_to_main": 9
-    },
-  ]; 
-function MergesGraph() {
+function MergesGraph({data}) {
+    console.log(data)
     //const [graphData, setGraphData] = useState();
     //const [loading, setLoading] = useState(true);
     //setGraphData(data);
