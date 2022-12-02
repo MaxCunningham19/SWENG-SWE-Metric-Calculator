a60ab1fdd04d89b588d5392217754633b3dcc302
AAjayiB <72974478+AAjayiB@users.noreply.github.com>
2022-12-02 04:10:21 +0000

    server receives post requests, doesn't get

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
 import React, {useEffect, useState} from 'react'
-
+import axios from 'axios'
+import { Button } from '@mui/material'
 function App() {
   const [backendData, setBackendData] = useState([{}])
 
-  useEffect(() => {
-    fetch('/api', {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-    },
-    }).then(
-    response => response.json()
-  ).then(
-    data => {
-      setBackendData(data)
-    }
-  )
-  }, [])
+  // useEffect(() => {
+  //   fetch('/api', {
+  //   method: 'GET',
+  //   headers: {
+  //       'Accept': 'application/json',
+  //   },
+  //   }).then(
+  //   response => response.json()
+  // ).then(
+  //   data => {
+  //     setBackendData(data)
+  //   }
+  // )
+  // }, [])
 
+  const data={repo_name:"MaxCunningham19/SWENG-SWE-Metric-Calculator",api_key:"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"}
+  const getRepo = async ()=>{
+    const {datan} = await axios.post('http://localhost:5000/api',data)
+    return datan
+
+  }
+  useEffect(()=>{
+        getRepo()
+    },[]);
   return(
-    <div>{JSON.stringify(backendData)}</div>
+    // <div>{JSON.stringify(backendData)}</div>
+    <Button onClick={()=>getRepo()}>Submit</Button>
+    
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 //const express = require('express')
 //const app = express()
 //
-
+const cors = require('cors')
 const express = require("express");
 const bodyParser = require("body-parser");
 const { spawn } = require('child_process');
@@ -9,10 +9,9 @@ const app = express();
 const fs = require("fs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(cors())
 async function uploadSize(repo, api) {
-
-    dataToSend = None
+    dataToSend = null
     succ = false
     // Reading Python files
     // spawn new child process to call the python script
@@ -22,6 +21,7 @@ async function uploadSize(repo, api) {
     // collect data from script
     python.stdout.on('data', function (data) {
         succ = true
+        console.log(data.toString())
         dataToSend = data.toString();
     });
 
@@ -30,28 +30,34 @@ async function uploadSize(repo, api) {
         console.error(`stderr: ${data}`);
     });
 
-    await new Promise((resolve) => {
-        python.on('exit', (code) => {
-            resolve();
-        })
+    // await new Promise((resolve) => {
+    //     python.on('exit', (code) => {
+    //         resolve();
+    //     })
 
-    });
+    // });
+    console.log("data to send: "+dataToSend)
+    console.log("status:" +succ)
+    return [dataToSend, succ]
 
-    return dataToSend, succ
 };
 
-app.get("/api", async (req, res) => {
-    var data, success = await uploadSize(req.body.repo_name, req.body.api_key);
+
+app.post("/api", async (req, res) => {
+    var [data, success] = await uploadSize(req.body.repo_name, req.body.api_key);
     if (success) {
-        console.log(data)
+        console.log("data: "+data)
+        
         res.sendStatus(200)
-        res.setHeader("Content-Type", "application/json");
-        res.json(JSON.parse(data));
+        // res.set("Content-Type", "application/json");
+        return res.json(data);
     } else {
-        res.setHeader("Content-Type", "application/json");
-        res.sendStatus(400).send(data.toString())
+        // res.setHeader("Content-Type", "application/json");
+        // return res.sendStatus(400).send(data.toString())
     }
 })
-
+app.get("/api",(req,res)=>{
+    
+    
+})
 app.listen(5000, () => { console.log("Server started on port 5000") })
-
