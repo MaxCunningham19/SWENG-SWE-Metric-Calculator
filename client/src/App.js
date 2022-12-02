2f8c051298298e29e37764980bab00b28a89d630
Max Cunningham <maxzinedine@gmail.com>
2022-12-02 12:58:34 +0000

    runs localy

--- client/src/App.js
+++ client/src/App.js
@@ -166,7 +166,7 @@ function App() {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': 'true'
     },
-      body: JSON.stringify({ title: 'React POST Request Example' })
+      body: JSON.stringify({ name:'MaxCunningham19/SWENG-SWE-Metric-Calculator',api: '' })
     };
     const response = await fetch('/api', requestOptions);
     const data = await response.json();
